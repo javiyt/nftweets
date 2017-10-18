5b39139fda7bff4acb3a50ee9351615190313cbb
Javi Martín <javiyt@users.noreply.github.com>
2017-10-18 13:14:57 +0200

    closes #18 Encrypt twitter key and secret (#22)
    * closes #18 Encrypt twitter key and secret
    
    * re #18 Encrypt twitter key and secret
    - Add encrypted keys when compiling from gradle
    - Add ndk to travis
    - Modify README to add new variables to run the project

--- app/src/main/java/yt/javi/nftweets/ui/main/MainActivity.kt
+++ app/src/main/java/yt/javi/nftweets/ui/main/MainActivity.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.util.LruCache
 import android.support.v7.app.AppCompatActivity
+import android.util.Base64
 import android.util.Log
 import com.beust.klaxon.Parser
 import com.twitter.sdk.android.core.DefaultLogger
-import com.twitter.sdk.android.core.Twitter
+import com.twitter.sdk.android.core.Twitter.initialize
 import com.twitter.sdk.android.core.TwitterAuthConfig
 import com.twitter.sdk.android.core.TwitterConfig
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,28 +20,63 @@ import yt.javi.nftweets.infrastructure.repositories.http.HttpReader
 import yt.javi.nftweets.ui.fragments.TeamListFragment
 import yt.javi.nftweets.ui.news.NewsFragment
 import java.net.URL
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
 
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        init {
+            System.loadLibrary("keys")
+        }
+
+        val ALGORITHM = "AES"
+    }
+
+    private external fun getCryptedTwitterKey(): String
+
+    private external fun getCryptedTwitterSecret(): String
+
+    private external fun getCryptedNewsApiKey(): String
+
+    private external fun getEncryptionKey(): String
+
+    private fun getDecryptedString(encryptedText: String): String {
+        val key = SecretKeySpec(Base64.decode(getEncryptionKey().toByteArray(), Base64.DEFAULT), ALGORITHM)
+        val cipher = Cipher.getInstance(ALGORITHM)
+        cipher.init(Cipher.DECRYPT_MODE, key)
+
+        return String(cipher.doFinal(Base64.decode(encryptedText, Base64.DEFAULT)))
+    }
+
+    private fun getTwitterKey(): String =
+            getDecryptedString(getCryptedTwitterKey())
+
+    private fun getTwitterSecret(): String =
+            getDecryptedString(getCryptedTwitterSecret())
+
+
+    private fun getNewsApiKey(): String =
+            getDecryptedString(getCryptedNewsApiKey())
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        Twitter.initialize(
+        initialize(
                 TwitterConfig.Builder(this)
                         .logger(DefaultLogger(Log.DEBUG))
                         .twitterAuthConfig(
                                 TwitterAuthConfig(
-                                        BuildConfig.TWITTER_CONSUMER_KEY,
-                                        BuildConfig.TWITTER_CONSUMER_SECRET
+                                        getTwitterKey(),
+                                        getTwitterSecret()
                                 )
                         )
                         .debug(true)
                         .build()
         )
-
 
         navigation.setOnNavigationItemSelectedListener { item ->
             var selectedFragment: Fragment? = null
@@ -52,7 +88,7 @@ class MainActivity : AppCompatActivity() {
                                         Parser(),
                                         HttpReader(LruCache(100))
                                 ),
-                                URL(BuildConfig.NEWS_API_URL)
+                                URL(BuildConfig.NEWS_API_URL + getNewsApiKey())
                         )
                 )
 
